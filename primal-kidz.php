bc79afd7e1d72d39f5e0c5bbe66b2e0324b4274e
Garypickles <me@garypickles.com>
2017-07-07 17:38:08 +0100

    kids thank you

--- primal-kidz.php
+++ primal-kidz.php
@@ -15,6 +15,37 @@
 
           <div class="kidz-hero">
             <img src="<?php echo get_template_directory_uri(); ?>/images/kidz-logo-both.svg" alt="Join The Club">
+
+            <!-- Kidz Monthley -->
+            <?php
+              if(pmpro_hasMembershipLevel(array('10', '11')))
+              { ?>
+
+                <div class="pk-info">
+
+                  <div class="info-wrapper">
+
+                    <div class="info-inner-wrapper">
+                      <h2>Thank you for choosing Primal&nbspKidz</h2>
+                      <p>Please contact your Cub leader via text/call <a href="tel:07805036614">07805&nbsp036614</a> or alternatively email us <a href="mailto:primaltraininguk@gmail.com">primaltraininguk@gmail.com</a></p>
+                    </div>
+
+                    <div class="rough-background">
+                      <img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/zerif-pro/images/rough-bg.png" alt="">
+                    </div>
+
+                  </div>
+
+                </div>
+
+              <?php
+            }else {
+              ?>
+
+
+            <?php } ?>
+            <!-- End Kidz Monthley -->
+
             <img src="<?php echo get_template_directory_uri(); ?>/images/chevron-arrow-down.png" alt="Join The Club">
           </div>
 
@@ -93,34 +124,6 @@
             <div class="sign-up-button">
               <a href="#pk-course-info" class="kidz-button"><img src="<?php echo get_template_directory_uri(); ?>/images/signuptoday.svg" alt="Join The Club"></a>
             </div>
-
-            <!-- Kidz Monthley -->
-            <?php
-              if(pmpro_hasMembershipLevel('10'))
-              { ?>
-
-
-              <?php
-            }else {
-              ?>
-
-
-            <?php } ?>
-            <!-- End Kidz Monthley -->
-
-            <!-- Kidz Weekley -->
-            <?php
-              if(pmpro_hasMembershipLevel('11'))
-              { ?>
-
-
-              <?php
-            }else {
-              ?>
-
-
-            <?php } ?>
-            <!-- End Kidz Weekley -->
 
           </div>
 
